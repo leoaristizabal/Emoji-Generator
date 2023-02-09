81355211d61225d6201ecbc619134010f538c4fa
Leo Aristizabal <68977686+leoaristizabal@users.noreply.github.com>
2023-02-09 00:19:15 -0400

    Add index files html, css y js.

---
+++ index.js
@@ -0,0 +1,8 @@
+const emoji = ["😀","😁","😂","😃","😄","😅","😆","😇","😈","😉","😊","😋","😌","😍","😎","😏","😔","😘",]
+
+const generateBtn = document.getElementById('btn');
+document.getElementById("emoji-container").textContent = emoji[0];
+generateBtn.addEventListener('click', function(){
+    let output = emoji[Math.floor (Math.random()*emoji.length)];
+    document.getElementById("emoji-container").textContent = output;
+})
